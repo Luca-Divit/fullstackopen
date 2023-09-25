87817c520bf559c03d6f73ec8ea08ecc959e0b3d
LucaDamiano DiVittorio <luca.div@hotmail.com>
2023-09-25 13:46:50 +0100

    Cleaned template and completed part1

--- part1/course-information/src/App.jsx
+++ part1/course-information/src/App.jsx
@@ -1,35 +1,59 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+const App = () => {
+  const course = "Half Stack application development";
+  const part1 = "Fundamentals of React";
+  const exercises1 = 10;
+  const part2 = "Using props to pass data";
+  const exercises2 = 7;
+  const part3 = "State of a component";
+  const exercises3 = 14;
 
-function App() {
-  const [count, setCount] = useState(0)
+  const Header = (props) => {
+    return <h1>{props.course}</h1>;
+  };
+
+  const Content = (props) => {
+    return (
+      <>
+        <p>
+          {props.part1} {props.exercises1}
+        </p>
+        <p>
+          {props.part2} {props.exercises2}
+        </p>
+        <p>
+          {props.part3} {props.exercises3}
+        </p>
+      </>
+    );
+  };
+
+  const Total = (props) => {
+    return (
+      <p>
+        Number of exercises{" "}
+        {props.exercises1 + props.exercises2 + props.exercises3}
+      </p>
+    );
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div>
+      <Header course={course} />
+      <Content
+        part1={part1}
+        part2={part2}
+        part3={part3}
+        exercises1={exercises1}
+        exercises2={exercises2}
+        exercises3={exercises3}
+      />
+      <Total
+        exercises1={exercises1}
+        exercises2={exercises2}
+        exercises3={exercises3}
+      />
+    </div>
+  );
+};
 
-export default App
+export default App;
