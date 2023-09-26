646a8334f5505ada90ad6c81f0d8679fbcd5a51d
LucaDamiano DiVittorio <luca.div@hotmail.com>
2023-09-26 12:40:15 +0100

    Refactored twice Firstly using an array instead of variables for the parts Secondly transforming everything into an object named course

--- part1/course-information/src/App.jsx
+++ part1/course-information/src/App.jsx
@@ -1,4 +1,5 @@
 const App = () => {
+  // First part using variables
   // const course = "Half Stack application development";
   // const part1 = "Fundamentals of React";
   // const exercises1 = 10;
@@ -7,61 +8,79 @@ const App = () => {
   // const part3 = "State of a component";
   // const exercises3 = 14;
 
-  const course = "Half Stack application development";
-  const part1 = {
-    name: "Fundamentals of React",
-    exercises: 10,
-  };
-  const part2 = {
-    name: "Using props to pass data",
-    exercises: 7,
-  };
-  const part3 = {
-    name: "State of a component",
-    exercises: 14,
+  // Second part using parts as an array
+  // const course = "Half Stack application development";
+  // const parts = [
+  //   {
+  //     name: "Fundamentals of React",
+  //     exercises: 10,
+  //   },
+  //   {
+  //     name: "Using props to pass data",
+  //     exercises: 7,
+  //   },
+  //   {
+  //     name: "State of a component",
+  //     exercises: 14,
+  //   },
+  // ];
+
+  // Third part using everything as an object named course
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
   };
 
   const Header = (props) => {
     // console.log(props);
-    return <h1>{props.course}</h1>;
+    return <h1>{props.course.name}</h1>;
   };
 
   const Part = (props) => {
     // console.log(props);
     return (
       <p>
-        {props.part.name} {props.part.exercise}
+        {props.name} {props.exercises}
       </p>
     );
   };
 
-  const Content = () => {
-    return (
-      <>
-        <Part part={part1} />
-        <Part part={part2} />
-        <Part part={part3} />
-      </>
-    );
+  const Content = (props) => {
+    // console.log(props.course.parts);
+    return props.course.parts.map((part) => {
+      return (
+        <Part key={part.name} name={part.name} exercises={part.exercises} />
+      );
+    });
   };
 
   const Total = (props) => {
     // console.log(props);
-    return (
-      <p>
-        Number of exercises{" "}
-        {props.exercises1.exercises +
-          props.exercises2.exercises +
-          props.exercises3.exercises}
-      </p>
-    );
+    let totalNumberOfExercises = 0;
+    for (let i = 0; i < props.course.parts.length; i++) {
+      totalNumberOfExercises += props.course.parts[i].exercises;
+    }
+    return <p>Number of exercises {totalNumberOfExercises}</p>;
   };
 
   return (
     <div>
       <Header course={course} />
-      <Content />
-      <Total exercises1={part1} exercises2={part2} exercises3={part3} />
+      <Content course={course} />
+      <Total course={course} />
     </div>
   );
 };
