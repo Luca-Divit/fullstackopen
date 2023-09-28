1e183d26ba4ea8046584afe48b0b700a5e4ec014
LucaDamiano DiVittorio <luca.div@hotmail.com>
2023-09-28 17:51:16 +0100

    Added using npm axios and json-server added script to run json-server shortcut added effectHook to get the persons form the json server at the start of the app

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,19 +1,26 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import axios from "axios";
 import Filter from "./components/Filter";
 import PersonsForm from "./components/PersonsForm";
 import Persons from "./components/Persons";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
   const [search, setSearch] = useState('');
 
+  useEffect(() => {
+    // console.log('calling the server');
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        // console.log(response);
+        setPersons(response.data)
+      });
+  }, []);
+
+  // console.log('We have this people', persons.length);
 
   const handleChangeName = (e) => {
     setNewName(e.target.value);
