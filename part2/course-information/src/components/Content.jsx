0af2a9f9c8e4e584e3731dc52be2a02055829bed
LucaDamiano DiVittorio <luca.div@hotmail.com>
2023-09-28 11:28:46 +0100

    Refactored Total and Component Using now inline reduce function to calculate the sum to pass to Total component

--- part2/course-information/src/components/Content.jsx
+++ part2/course-information/src/components/Content.jsx
@@ -2,16 +2,14 @@ import Part from "./Part";
 import Total from "./Total";
 
 const Content = ({ parts }) => {
-  let sum = 0
   return (
     <>
       {parts.map(part => {
-        sum += part.exercises
         return (
           <Part key={part.id} part={part} />
         )
       })}
-      <Total sum={sum} />
+      <Total sum={parts.reduce((sum,part) => sum += part.exercises, 0)} />
     </>
   );
 };
