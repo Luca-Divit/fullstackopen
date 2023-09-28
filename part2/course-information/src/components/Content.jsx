9c64a255c3bee122049058d99a9fa0f1758f1c99
LucaDamiano DiVittorio <luca.div@hotmail.com>
2023-09-28 11:15:23 +0100

    Added the sum to the page Created the component Total and passed it inside the component Content Created a let variable called sum that stores the total and updated in every map iteration and lastly passed the sum to the component Total

--- part2/course-information/src/components/Content.jsx
+++ part2/course-information/src/components/Content.jsx
@@ -1,17 +1,20 @@
 import Part from "./Part";
+import Total from "./Total";
 
 const Content = ({ parts }) => {
+  let sum = 0
   return (
     <>
       {parts.map(part => {
+        sum += part.exercises
         return (
           <Part key={part.id} part={part} />
         )
       })}
+      <Total sum={sum} />
     </>
-  )
+  );
+};
 
-}
 
-
-export default Content
+export default Content;
--- part2/course-information/src/components/Header.jsx
+++ part2/course-information/src/components/Header.jsx
@@ -1,3 +1,3 @@
 const Header = ({ course }) => <h1>{course}</h1>;
 
-export default Header
+export default Header;
