710a32129011b76e8e9924549fbf3bf32997fb39
LucaDamiano DiVittorio <luca.div@hotmail.com>
2023-09-28 11:05:28 +0100

    Created Header Content Part components refactored the code in the Part component using map to create dynamically the tags using the data

---
+++ part2/course-information/src/components/Course.jsx
@@ -0,0 +1,15 @@
+// import { useState } from "react";
+import Header from "./Header";
+import Content from "./Content";
+
+const Course = ({course}) => {
+  // console.log(props)
+  return (
+    <>
+      <Header course={course.name}/>
+      <Content parts={course.parts} />
+    </>
+  )
+};
+
+export default Course;
