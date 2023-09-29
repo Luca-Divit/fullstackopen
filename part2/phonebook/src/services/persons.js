033964c8250c81ad7f8a6d97aec8431c00b2dc84
LucaDamiano DiVittorio <luca.div@hotmail.com>
2023-09-29 10:10:14 +0100

    Added eplanatory comment in the persons.js file

--- part2/phonebook/src/services/persons.js
+++ part2/phonebook/src/services/persons.js
@@ -1,12 +1,15 @@
 import axios from "axios";
 
+// Base url for all the api calls
 const baseUrl = 'http://localhost:3001/persons';
 
+// GET all the people from the db
 const getAll = () => {
   const request = axios.get(baseUrl)
     return request.then(response => response.data)
 }
 
+// Persist a new person in the db
 const create = newNote => {
   const request = axios.post(baseUrl, newNote)
     return request.then(response => response.data);
