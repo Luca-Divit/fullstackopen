1a6958d140fefeffa949f0c4695a3cacf2edc818
LucaDamiano DiVittorio <luca.div@hotmail.com>
2023-09-30 12:16:34 +0100

    Added search functionality • Added input and ul to show results • When typing the handleChange event take the text makes a regex and return the matching countries name into the state of the component Countries • Conditional rendering that shows countries only when the search return less than 11

--- part2/data-for-countries/src/App.jsx
+++ part2/data-for-countries/src/App.jsx
@@ -1,4 +1,42 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
 const App = () => {
+  const [allCountries, setAllCountries] = useState([]);
+  const [search, setSearch] = useState('');
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    const baseUrl = 'https://studies.cs.helsinki.fi/restcountries/api/all'
+    axios
+      .get(baseUrl)
+      .then(response => response.data)
+      .then(coutries => setAllCountries(coutries))
+  },[]);
+
+  const handleChange = (e) => {
+    const typed = e.target.value;
+    setSearch(typed);
+    const regex = new RegExp(typed, 'gi')
+
+    const filteredCountries = allCountries.filter(country => {
+      return country.name.common.match(regex) ;
+    });
+    setCountries(filteredCountries);
+  }
+
+  return (
+    <div>
+      <input value={search} onChange={handleChange} />
+      {countries.length < 11 ?
+        <ul>
+          {countries.map((c,i) => <li key={i}>{c.name.common}</li>)}
+        </ul> :
+        <p>Too many countries specify more</p>
+      }
+
+    </div>
+  )
 }
 
 export default App
