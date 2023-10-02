e11f0e4c457430420a2ddf4c23b6d9c370f68836
LucaDamiano DiVittorio <luca.div@hotmail.com>
2023-10-02 11:32:43 +0100

    Added button to select what country to show • When more than 1 and less than 11 matches, added button to select what country to show

--- part2/data-for-countries/src/App.jsx
+++ part2/data-for-countries/src/App.jsx
@@ -5,6 +5,7 @@ const App = () => {
   const [allCountries, setAllCountries] = useState([]);
   const [search, setSearch] = useState('');
   const [countries, setCountries] = useState([]);
+  const [countryToShow, setCountryToShow] = useState(null);
 
   useEffect(() => {
     const baseUrl = 'https://studies.cs.helsinki.fi/restcountries/api/all'
@@ -13,6 +14,11 @@ const App = () => {
       .then(response => response.data)
       .then(coutries => setAllCountries(coutries))
   },[]);
+
+  useEffect(() => {
+    setCountryToShow(null)
+  }, [search])
+
 
   const handleChange = (e) => {
     const typed = e.target.value;
@@ -27,9 +33,6 @@ const App = () => {
 
   const handleDisplay = (places) => {
     if (places.length === 1) {
-      // const l = places[0]
-      // console.log(places[0]);
-      // debugger
       return (
         <div>
           <h1>{places[0].name.common}</h1>
@@ -42,19 +45,33 @@ const App = () => {
           <img src={places[0].flags.png} alt={`${places[0].name.common} flag`} />
         </div>
       )
-    } else if (countries.length < 11) {
+    }
+    if (countryToShow) {
       return (
-        <ul>
-          {countries.map((c,i) => <li key={i}>{c.name.common}</li>)}
-        </ul>
+        <div>
+          <h1>{countryToShow.name.common}</h1>
+          <div>Capital: {countryToShow.capital.map((c, i) => <span key={i}>{c}</span>)}</div>
+          <div>Area: {countryToShow.area}</div>
+          <h3>Languages</h3>
+          <ul>
+            {Object.values(countryToShow.languages).map((lang, i) => <li key={i}>{lang}</li>)}
+          </ul>
+          <img src={countryToShow.flags.png} alt={`${countryToShow.name.common} flag`} />
+        </div>
       )
     }
+    return (
+      <ul>
+        {countries.map((c,i) => <li key={i}>{c.name.common} <button onClick={() => setCountryToShow(c)}>show</button></li>)}
+      </ul>
+    )
   }
 
   return (
     <div>
       <p>find coutries <input value={search} onChange={handleChange} /></p>
-      {countries.length < 11 ?
+      {
+        countries.length < 11 ?
         handleDisplay(countries) :
         <p>Too many matches, specify another filter</p>
       }
