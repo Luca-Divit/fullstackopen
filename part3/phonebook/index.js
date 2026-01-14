97927e433f5a51421883047cac256f4347db5ed7
LucaDamiano DiVittorio <luca.div@hotmail.com>
2026-01-14 00:01:57 +0000

    step 2 and 3 of the api exercise

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -28,8 +28,24 @@ app.get("/", (req, res) => {
   res.send("<h1>Hello World</h1>");
 });
 
+app.get("/info", (req, res) => {
+  const numOfPersons = persons.length;
+  // console.log(numOfPersons);
+  const today = Date();
+  // console.log(today);s
+  const content = `<p>Phonebook has content for ${numOfPersons} people</p><p>${today}</p>`;
+  res.send(content);
+});
+
 app.get("/api/persons", (req, res) => {
   res.json(persons);
+});
+
+app.get("/api/persons/:id", (req, res) => {
+  const id = req.params.id;
+  // console.log(id);
+  const person = persons.find((p) => p.id === id);
+  person ? res.json(person) : res.status(404).end();
 });
 
 const PORT = 3001;
