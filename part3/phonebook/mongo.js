5b7d642109c5e25810e4d6932c4bed5bdad97b17
LucaDamiano DiVittorio <luca.div@hotmail.com>
2026-01-24 20:04:01 +0000

    part3.12 adding mongodb into our phonebook backend and handle storing new person and retrieve all the people

---
+++ part3/phonebook/mongo.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose");
+
+if (process.argv.length < 3) {
+  console.log("Please input the password");
+  process.exit(1);
+}
+
+const password = process.argv[2];
+
+const url = `mongodb+srv://dbuser:${password}@cluster0.lru0dfz.mongodb.net/?appName=Cluster0`;
+
+mongoose.set("strictQuery", false);
+
+mongoose.connect(url, { family: 4 });
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+});
+
+const Person = mongoose.model("Person", personSchema);
+
+if (process.argv.length === 5) {
+  const name = process.argv[3];
+  const number = process.argv[4];
+
+  person = new Person({
+    name,
+    number,
+  });
+
+  person.save().then((res) => {
+    console.log(`added ${name} number ${number} to phonebook`);
+    mongoose.connection.close();
+  });
+}
+
+if (process.argv.length !== 5) {
+  Person.find({}).then((res) => {
+    console.log("phonebook:");
+    res.forEach((p) => console.log(`${p.name} ${p.number}`));
+    mongoose.connection.close();
+  });
+}
