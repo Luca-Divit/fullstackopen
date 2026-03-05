24c6db3a648157295df005fb52fcba41303b967d
LucaDamiano DiVittorio <luca.div@hotmail.com>
2026-03-05 23:49:30 +0000

    added test for get blogs

--- part4/bloglist/utils/config.js
+++ part4/bloglist/utils/config.js
@@ -1,6 +1,9 @@
 require("dotenv").config();
 
 const PORT = process.env.PORT;
-const MONGODB_URI = process.env.MONGODB_URI;
+const MONGODB_URI =
+  process.env.NODE_ENV === "test"
+    ? process.env.MONGODB_URI_TEST
+    : process.env.MONGODB_URI;
 
 module.exports = { PORT, MONGODB_URI };
