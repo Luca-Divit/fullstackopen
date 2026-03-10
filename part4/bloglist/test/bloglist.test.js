6055cf5de9937b903d4fb8dd19997a9f2b8b5281
LucaDamiano DiVittorio <luca.div@hotmail.com>
2026-03-10 01:13:35 +0000

    blog have ids test

--- part4/bloglist/test/bloglist.test.js
+++ part4/bloglist/test/bloglist.test.js
@@ -1,31 +1,12 @@
 const { test, after, beforeEach } = require("node:test");
+const assert = require("node:assert");
 const mongoose = require("mongoose");
 const supertest = require("supertest");
 const app = require("../app");
 const Blog = require("../models/blog");
+const initialBlogs = require("./test_helper");
 
 const api = supertest(app);
-
-const initialBlogs = [
-  {
-    title: "My first blog",
-    author: "Me",
-    url: "localhost",
-    likes: 1,
-  },
-  {
-    title: "My second blog",
-    author: "Myself",
-    url: "localhosttwo",
-    likes: 2,
-  },
-  {
-    title: "My third blog",
-    author: "and I",
-    url: "localhostthree",
-    likes: 3,
-  },
-];
 
 beforeEach(async () => {
   await Blog.deleteMany({});
@@ -35,10 +16,17 @@ beforeEach(async () => {
 });
 
 test.only("return the correct number of blog posts in the json format", async () => {
-  await api
-    .get("/api/blogs")
-    .expect(200)
-    .expect("Content-Type", /application\/json/);
+  const response = await api.get("/api/blogs");
+
+  assert.strictEqual(response.body.length, 3);
+});
+
+test.only("the unique identifier property of the blog posts is named id", async () => {
+  const response = await api.get("/api/blogs");
+
+  blogHaveIds = response.body.every((b) => b.id);
+
+  assert.strictEqual(blogHaveIds, true);
 });
 
 after(async () => {
---
+++ part4/bloglist/test/test_helper.js
@@ -0,0 +1,22 @@
+const initialBlogs = [
+  {
+    title: "My first blog",
+    author: "Me",
+    url: "localhost",
+    likes: 1,
+  },
+  {
+    title: "My second blog",
+    author: "Myself",
+    url: "localhosttwo",
+    likes: 2,
+  },
+  {
+    title: "My third blog",
+    author: "and I",
+    url: "localhostthree",
+    likes: 3,
+  },
+];
+
+module.exports = initialBlogs;
