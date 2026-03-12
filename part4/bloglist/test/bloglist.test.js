a39fe7a3b7e9530f1bcb14b06e065f179652263d
LucaDamiano DiVittorio <luca.div@hotmail.com>
2026-03-12 00:54:40 +0000

    created test for post request and refactor api call using async func

--- part4/bloglist/test/bloglist.test.js
+++ part4/bloglist/test/bloglist.test.js
@@ -15,18 +15,39 @@ beforeEach(async () => {
   await Promise.all(promiseArray);
 });
 
-test.only("return the correct number of blog posts in the json format", async () => {
+test("return the correct number of blog posts in the json format", async () => {
   const response = await api.get("/api/blogs");
 
   assert.strictEqual(response.body.length, 3);
 });
 
-test.only("the unique identifier property of the blog posts is named id", async () => {
+test("the unique identifier property of the blog posts is named id", async () => {
   const response = await api.get("/api/blogs");
 
   blogHaveIds = response.body.every((b) => b.id);
 
   assert.strictEqual(blogHaveIds, true);
+});
+
+test.only("correctly store a new blog in the database", async () => {
+  const newBlog = new Blog({
+    title: "New blog",
+    author: "New author",
+    url: "www.newurl.com",
+    likes: 0,
+  });
+
+  await api
+    .post("/api/blogs")
+    .send(newBlog)
+    .expect(201)
+    .expect("Content-Type", /application\/json/);
+
+  const blogs = await api.get("/api/blogs");
+  const contents = blogs.body.map((b) => b.url);
+
+  assert.strictEqual(blogs.body.length, initialBlogs.length + 1);
+  assert(contents.includes("www.newurl.com"));
 });
 
 after(async () => {
