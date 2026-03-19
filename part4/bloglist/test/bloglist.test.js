c1b8513df4fdf1e95b91e0d65201d0d1a4399ea7
LucaDamiano DiVittorio <luca.div@hotmail.com>
2026-03-19 01:08:56 +0000

    refactored tests adding describe blocks

--- part4/bloglist/test/bloglist.test.js
+++ part4/bloglist/test/bloglist.test.js
@@ -1,4 +1,4 @@
-const { test, after, beforeEach } = require("node:test");
+const { test, after, beforeEach, afterEach, describe } = require("node:test");
 const assert = require("node:assert");
 const mongoose = require("mongoose");
 const supertest = require("supertest");
@@ -15,67 +15,71 @@ beforeEach(async () => {
   await Promise.all(promiseArray);
 });
 
-test("return the correct number of blog posts in the json format", async () => {
-  const response = await api.get("/api/blogs");
+describe("GET blogs", () => {
+  test("return the correct number of blog posts in the json format", async () => {
+    const response = await api.get("/api/blogs");
 
-  assert.strictEqual(response.body.length, 3);
-});
-
-test("the unique identifier property of the blog posts is named id", async () => {
-  const response = await api.get("/api/blogs");
-
-  blogHaveIds = response.body.every((b) => b.id);
-
-  assert.strictEqual(blogHaveIds, true);
-});
-
-test("correctly store a new blog in the database", async () => {
-  const newBlog = {
-    title: "New blog",
-    author: "New author",
-    url: "www.newurl.com",
-    likes: 0,
-  };
-
-  await api
-    .post("/api/blogs")
-    .send(newBlog)
-    .expect(201)
-    .expect("Content-Type", /application\/json/);
-
-  const blogs = await api.get("/api/blogs");
-  const contents = blogs.body.map((b) => b.url);
-
-  assert.strictEqual(blogs.body.length, initialBlogs.length + 1);
-  assert(contents.includes("www.newurl.com"));
-});
-
-test("if likes property is missing in request then default to 0", async () => {
-  const newBlog = {
-    title: "New blog",
-    author: "New author",
-    url: "www.newurl.com",
-  };
-
-  await api
-    .post("/api/blogs")
-    .send(newBlog)
-    .expect(201)
-    .expect("Content-Type", /application\/json/);
-
-  const blogs = await api.get("/api/blogs");
-  const lastSavedBlog = blogs.body.slice(-1)[0];
-
-  assert.strictEqual(lastSavedBlog.likes, 0);
-});
-
-test("missing title or url in a post request will respond 400", async () => {
-  const newBlog = new Blog({
-    author: "New author",
-    url: "www.newurl.com",
+    assert.strictEqual(response.body.length, 3);
   });
 
-  await api.post("/api/blogs").send(newBlog).expect(400);
+  test("the unique identifier property of the blog posts is named id", async () => {
+    const response = await api.get("/api/blogs");
+
+    blogHaveIds = response.body.every((b) => b.id);
+
+    assert.strictEqual(blogHaveIds, true);
+  });
+});
+
+describe("POST blogs", () => {
+  test("correctly store a new blog in the database", async () => {
+    const newBlog = {
+      title: "New blog",
+      author: "New author",
+      url: "www.newurl.com",
+      likes: 0,
+    };
+
+    await api
+      .post("/api/blogs")
+      .send(newBlog)
+      .expect(201)
+      .expect("Content-Type", /application\/json/);
+
+    const blogs = await api.get("/api/blogs");
+    const contents = blogs.body.map((b) => b.url);
+
+    assert.strictEqual(blogs.body.length, initialBlogs.length + 1);
+    assert(contents.includes("www.newurl.com"));
+  });
+
+  test("if likes property is missing in request then default to 0", async () => {
+    const newBlog = {
+      title: "New blog",
+      author: "New author",
+      url: "www.newurl.com",
+    };
+
+    await api
+      .post("/api/blogs")
+      .send(newBlog)
+      .expect(201)
+      .expect("Content-Type", /application\/json/);
+
+    const blogs = await api.get("/api/blogs");
+    const lastSavedBlog = blogs.body.slice(-1)[0];
+
+    assert.strictEqual(lastSavedBlog.likes, 0);
+  });
+
+  test("missing title or url in a post request will respond 400", async () => {
+    const newBlog = new Blog({
+      author: "New author",
+      url: "www.newurl.com",
+    });
+
+    await api.post("/api/blogs").send(newBlog).expect(400);
+  });
 });
 
 after(async () => {
