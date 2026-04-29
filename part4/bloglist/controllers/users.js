a206cb81793492d2bb8f3701dfbe7455d0192626
LucaDamiano DiVittorio <luca.div@hotmail.com>
2026-04-30 00:46:15 +0100

    added users controller and post create new user, also bumped pkgs versions

---
+++ part4/bloglist/controllers/users.js
@@ -0,0 +1,18 @@
+const userRouter = require("express").Router();
+const User = require("../models/user");
+const bcrypt = require("bcrypt");
+
+userRouter.post("/", async (req, res) => {
+  const { username, name, password } = req.body;
+  const passwordHash = bcrypt.hash(password, 10);
+  const user = new User(username, name, passwordHash);
+
+  if (!user.username || !user.name || !user.passwordHash) {
+    return res
+      .status(400)
+      .json({ error: "Username, name and password must be present" });
+  }
+
+  const result = await user.save();
+  res.status(201).json(result);
+});
